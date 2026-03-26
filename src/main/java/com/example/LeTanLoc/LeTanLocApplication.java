2d8fc3f06d5c746360f135fcf1c64902fdaa87cb
locle1010 <letanlochhgg@gmail.com>
2026-03-26 14:20:21 +0700

    bai7

---
+++ src/main/java/com/example/LeTanLoc/LeTanLocApplication.java
@@ -0,0 +1,13 @@
+package com.example.LeTanLoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeTanLocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeTanLocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LeTanLoc/LeTanLocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LeTanLoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeTanLocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
